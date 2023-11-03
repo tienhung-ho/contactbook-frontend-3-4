e2990e669cda3beadfa004aa35c3aa7db3917abb
Ho Hung <hohung1203@gmail.com>
2023-11-03 16:00:30 +0700

    Create pages update contact

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,14 @@ const routes = [
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
-  }
+  },
+
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ]
 
 const router = createRouter({

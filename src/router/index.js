945490c2dd95db43943ee7470a4e5d058e6432ae
Ho Hung <hohung1203@gmail.com>
2023-11-03 16:21:42 +0700

    Completed!

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,12 @@ const routes = [
     component: () => import("@/views/ContactEdit.vue"),
     props: true, // Truyền các biến trong $route.params vào làm props
   },
+  {
+    path: "/contacts/create",
+    name: "contact.add",
+    component: () => import("@/views/CreateContact.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ]
 
 const router = createRouter({
